3ee99b04379d174b3b6c369f59fb9ae794d9dec2
martinmolina7k <romartin.1234@gmail.com>
2026-06-25 10:42:16 -0300

    Add files via upload

---
+++ [MartinMolina]-Act9/Punto 1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Punto_1
+{
+    internal class Program
+    {
+        //1. Se desea desarrollar un programa que permita registrar los nombres y las
+        //calificaciones de 6 estudiantes.Luego de cargar los datos, se debe mostrar el
+        //nombre del estudiante con la nota más alta, junto con su nota.Al igual que el
+        //estudiante con la nota más baja.Informar si hay estudiantes con la misma nota
+        //máxima o mínima.
+        private string[] nombres;
+        private float[] califig;
+
+        public void Cargar()
+        {
+            nombres = new string[6];
+            califig = new float[6];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("ingrese el nombre del alumno: ");
+                nombres[f] = Console.ReadLine();
+                Console.Write("inngrese la nota: ");
+                string linea;
+                linea = Console.ReadLine();
+                califig[f] = float.Parse(linea);
+
+            }
+
+        }
+
+        public void mayornota()
+        {
+
+            float mayor;
+            int pos;
+
+            mayor = califig[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (califig[f] > mayor)
+                {
+                    mayor = califig[f];
+                    pos = f;
+                }
+
+
+            }
+
+            Console.WriteLine("alumno con mayor nota es: " + nombres[pos]);
+            Console.WriteLine("tiene una nota de: " + mayor);
+
+        }
+        public void menornota()
+        {
+            float menor;
+            int pos;
+
+            menor = califig[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (califig[f] < menor)
+                {
+                    menor = califig[f];
+                    pos = f;
+                }
+            }
+            Console.WriteLine("alumno con menor nota es: " + nombres[pos]);
+            Console.WriteLine("tiene una nota de: " + menor);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program pv = new Program();
+            pv.Cargar();
+            pv.mayornota();
+            pv.menornota();
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ [MartinMolina]-Act9/Punto 3/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Punto_3
+{
+    internal class Program
+    {
+        //3. Se registran los nombres de 5 atletas y sus tiempos (en segundos) en una
+        //carrera de 100 metros.El programa debe cargar los datos en dos vectores
+        //paralelos, calcular y mostrar el promedio de los tiempos, mostrar el nombre del
+        //atleta con mejor y peor tiempo, y mostrar los nombres de quienes superaron el
+        //promedio.
+
+        private string[] nombres;
+        private double[] tiempo;
+        
+
+        public void cargar()
+        {
+            nombres = new string[5];
+            tiempo = new double[100];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("ingrese el nombre del atleta: ");
+                nombres[f] = Console.ReadLine();
+                Console.Write("inngrese el tiempo: ");
+                string linea;
+                linea = Console.ReadLine();
+                tiempo[f] = double.Parse(linea);
+            }
+        }
+        public void promedio()
+        {
+            double suma = 0;
+
+            for (int i = 0; i < tiempo.Length; i++)
+            {
+                suma += tiempo[i];
+            }
+
+            double promedio = suma / tiempo.Length;
+
+            Console.WriteLine("el promedio es: " + promedio);
+            Console.WriteLine("Superaron el promedio: ");
+            for (int i = 0; i < tiempo.Length; i++) ;
+            {
+                int f = 0;
+
+                if (tiempo[f] < promedio)
+                {
+                    Console.WriteLine(nombres[f]);
+                }
+
+            }
+        }
+
+
+        public void atmejor()
+        {
+
+            double atmejor;
+            int pos;
+
+            atmejor = tiempo[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (tiempo[f] < atmejor)
+                {
+                    atmejor = tiempo[f];
+                    pos = f;
+                }
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("el atleta: " + nombres[pos]);
+            Console.WriteLine("tiene una puntuacion mejor: " + atmejor);
+        }
+        public void atpeor()
+        {
+            double atpeor;
+            int pos;
+
+            atpeor = tiempo[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (tiempo[f] > atpeor)
+                {
+                    atpeor = tiempo[f];
+                    pos = f;
+                }
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("el atleta: " + nombres[pos]);
+            Console.WriteLine("tiene una puntuacion peor: " + atpeor);
+            Console.WriteLine();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program ats = new Program();
+            ats.cargar();
+            ats.atmejor();
+            ats.atpeor();
+            ats.promedio();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
