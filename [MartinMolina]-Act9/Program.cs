7ed7d1795b318759181dd00201d9c14d0e3dbc66
martinmolina7k <romartin.1234@gmail.com>
2026-06-25 10:21:11 -0300

    Add files via upload

---
+++ [MartinMolina]-Act9/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Punto_4
+{
+    //   4. Se realiza una evaluación a 6 docentes por parte de sus alumnos.Se registran
+    //sus nombres y puntajes promedio obtenidos (de 1 a 10).
+
+    //Cargar sus datos en vectores paralelos, mostrar docente con calificación más
+    //alta y más baja, ordenar los vectores de mayor a menor de acuerdo con la
+    //calificación y mostrar en pantalla la cantidad de docentes que aprobaron y
+    //desaprobaron(tomando como base que se aprueba con una nota mayor o igual
+    //a 6)
+    internal class Program
+    {
+        private string[] nombres;
+        private float[] puntaje;
+
+
+        public void cargar()
+        {
+            nombres = new string[6];
+            puntaje = new float[10];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("ingrese el nombre del docente: ");
+                nombres[f] = Console.ReadLine();
+                Console.Write("inngrese el puntaje: ");
+                string linea;
+                linea = Console.ReadLine();
+                puntaje[f] = float.Parse(linea);
+            }
+        }
+
+        public void promedio()
+        {
+            double suma = 0;
+
+            for (int i = 0; i < puntaje .Length; i++)
+            {
+                suma += puntaje [i];
+            }
+
+            double promedio = suma / puntaje.Length;
+
+            Console.WriteLine();
+            Console.WriteLine("el promedio de los docentes es: " + promedio);
+            Console.WriteLine();
+
+            for (int i = 0; i < puntaje.Length; i++) ;
+            {
+                int f = 0;
+                if (puntaje[f] < promedio)
+                {
+                    Console.WriteLine(nombres[f]);
+                }
+
+            }
+        }
+
+        public void docalto()
+        {
+
+           float docalto;
+            int pos;
+
+            docalto = puntaje[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (puntaje[f] < docalto)
+                {
+                    docalto = puntaje[f];
+                    pos = f;
+                }
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("el docente: " + nombres[pos]);
+            Console.WriteLine("tiene un puntaje alto de: " + docalto);
+            Console.WriteLine("-------------------------");
+        }
+        public void docbajo()
+        {
+            float docbajo;
+            int pos;
+
+            docbajo = puntaje[0];
+            pos = 0;
+            for (int f = 1; f < nombres.Length; f++)
+            {
+                if (puntaje[f] > docbajo)
+                {
+                    docbajo = puntaje[f];
+                    pos = f;
+                }
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("el docente: " + nombres[pos]);
+            Console.WriteLine("tiene un puntaje bajo de: " + docbajo);
+            Console.WriteLine("-------------------------");
+        }
+
+        public void desaprob()
+        {
+            Console.WriteLine("--------desaprobaron--------");
+            
+            for (int i = 6; i > nombres.Length; i++)
+            {
+                Console.WriteLine(puntaje[i]);
+            }
+            Console.WriteLine("");
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Program dc = new Program();
+            dc.cargar();
+            dc.promedio();
+            dc.docalto();
+            dc.docbajo();
+            dc.desaprob();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [MartinMolina]-Act9/Punto 2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Punto_2
+{
+    internal class Program
+    {
+        //2. Una empresa registra los nombres de sus 5 vendedores y el total de ventas
+        //realizadas por cada uno en un mes.Cargar los nombres y ventas en dos
+        //vectores paralelos, ordenar los datos de mayor a menor según las ventas,
+        //imprimir la lista ordenada con nombre y monto de la venta, e informar quien fue
+        //el que menos vendió de los 5 empleados.
+        private string[] nombres;
+        private double[] ventas;
+
+        public void cargar()
+        {
+            nombres = new string[5];
+            ventas = new double[5];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("ingrese el nombre del empleado:");
+                nombres[f] = Console.ReadLine();
+                Console.Write("ingrese sueldo:");
+                string linea;
+                linea = Console.ReadLine();
+                ventas[f] = double.Parse(linea);
+            }
+        }
+        public void ordenar()
+        {
+            for (int i = 0; i < ventas.Length - 1; i++)
+            {
+                for (int k = i; k < ventas.Length; k++)
+                {
+                    if (ventas[i] < ventas[k])
+                    {
+                        double auxventa = ventas[i];
+                        ventas[i] = ventas[k];
+                        ventas[k] = auxventa;
+
+                        string auxnom = nombres[i];
+                        nombres[i] = nombres[k];
+                        nombres[k] = auxnom;
+                    }
+                }
+            }
+
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine();
+            Console.WriteLine("lista ordenada:");
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i] + " = " + ventas[i]);
+            }
+
+            Console.WriteLine("menor vendedor: " + nombres[nombres.Length - 1]);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program gg = new Program();
+            gg.cargar();
+            gg.ordenar();
+            gg.imprimir();
+            Console.ReadKey();
+
+        }
+    }
+}
