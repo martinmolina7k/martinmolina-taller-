ba2fb3d3eb47c3a53eb74eac8dab0e820e189da4
martinmolina7k <romartin.1234@gmail.com>
2026-08-21 11:23:29 -0300

    Add files via upload

---
+++ [MartinMolina]-Act22/Punto 1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Punto1
+{ /*Actividad 1: Control de horarios en un gimnasio
+Problema:
+Crear una clase ClaseGimnasio con atributos: nombreClase, horaInicio y horaFin (usar
+DateTime).
+ Implementar un constructor que permita cargar los datos desde consola y otro
+que cargue valores por defecto (sobrecarga de constructores).
+ Incluir un método para calcular la duración de la clase usando TimeSpan.
+ Crear un vector de 3 clases de gimnasio y mostrar:
+1. La clase que tenga la mayor duración.
+2. El nombre y el horario de inicio de la clase más temprana.*/
+
+    internal class ClaseGimnasio
+    {
+        private string nombreClase;
+        private DateTime horaInicio;
+        private DateTime horaFin;
+
+        public string NombreClase { get { return nombreClase; } set { nombreClase = value; } }
+        public DateTime HoraInicio { get { return horaInicio; } set { horaInicio = value; } }
+        public DateTime HoraFin { get { return horaFin; } set { horaFin = value; } }
+
+        public ClaseGimnasio()
+        {
+            Console.WriteLine("Ingrese el nombre de la clase");
+            NombreClase = Console.ReadLine();
+            Console.WriteLine("Ingrese la hora de inicio");
+            HoraInicio = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la hora de salida");
+            HoraFin = DateTime.Parse(Console.ReadLine());
+        }
+
+
+        public ClaseGimnasio(string nombreClase, DateTime horaInicio, DateTime horaFin)
+        {
+
+            this.NombreClase = nombreClase;
+            this.HoraInicio = horaInicio;
+            this.HoraFin = horaFin;
+        }
+
+        public TimeSpan DuracionClase()
+        {
+            TimeSpan Duracion = HoraFin - HoraInicio;
+            return Duracion;
+
+        }
+
+        static void Main(string[] args)
+        {
+            ClaseGimnasio[] Clase = new ClaseGimnasio[3];
+
+            for (int i = 0; i < Clase.Length; i++)
+            {
+                Clase[i] = new ClaseGimnasio();
+
+            }
+
+            ClaseGimnasio claseMasLarga = Clase[0];
+            ClaseGimnasio claseMasTemprana = Clase[0];
+            foreach (var clase in Clase)
+            {
+                if (clase.DuracionClase() > claseMasLarga.DuracionClase())
+                {
+                    claseMasLarga = clase;
+
+                }
+                if (clase.HoraInicio < claseMasTemprana.HoraInicio)
+                {
+                    claseMasTemprana = clase;
+                }
+            }
+            Console.WriteLine($"La clase con mayor duracion es: {claseMasLarga.NombreClase}, con una duracion de {claseMasLarga.DuracionClase()}");
+            Console.WriteLine($"La clase mas temprana es: {claseMasTemprana.NombreClase}, iniciando a las {claseMasTemprana.HoraInicio}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [MartinMolina]-Act22/Punto 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    /*Actividad 2: Posicionamiento de elementos en consola
+Problema:
+Definir una clase ElementoPantalla con atributos: nombre, posX y posY.
+ Implementar propiedades y un constructor que cargue valores.
+ Crear un método Mostrar() que use Console.SetCursorPosition() para ubicar el
+nombre en pantalla y Console.CursorVisible para ocultar el cursor.
+ Generar un vector de 4 elementos y mostrarlos en distintas posiciones en la
+consola.*/
+
+    internal class ElementoPantalla
+    {
+        private string nombre;
+        private int posX;
+        private int posY;
+
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int PosX { get { return posX; } set { posX = value; } }
+        public int PosY { get { return posY; } set { posY = value; } }
+
+        public ElementoPantalla(string nombre, int posX, int posY)
+        {
+            this.Nombre = nombre;
+            this.PosX = posX;
+            this.PosY = posY;
+        }
+
+        public void Mostrar()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(PosX, PosY);
+            Console.Write(Nombre);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElementoPantalla[] elementos = new ElementoPantalla[4];
+
+            for (int i = 0; i < elementos.Length; i++)
+            {
+                Console.WriteLine("Ingrese el nombre a mostrar");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese en que posicion horizontal");
+                int posx = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese en que posicion vertical");
+                int posy = int.Parse(Console.ReadLine());
+                ElementoPantalla elemento = new ElementoPantalla(nombre, posx, posy);
+                elementos[i] = elemento;
+            }
+
+
+            foreach (var elemento in elementos)
+            {
+                elemento.Mostrar();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
